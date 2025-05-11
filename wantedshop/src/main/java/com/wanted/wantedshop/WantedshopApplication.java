d4b8cc094806ec346cbde83baefd8c0e411c523b
wogjs0911 <wogjs0911@naver.com>
2025-05-11 19:22:43 +0900

    fix: Spring Data JPA의 Auditing 기능 추가

--- wantedshop/src/main/java/com/wanted/wantedshop/WantedshopApplication.java
+++ wantedshop/src/main/java/com/wanted/wantedshop/WantedshopApplication.java
@@ -2,12 +2,12 @@ package com.wanted.wantedshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WantedshopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WantedshopApplication.class, args);
 	}
-
 }
